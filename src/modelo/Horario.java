5f56c6c0bd4023c700b36b5059334918aea071ab
DreiII664 <diegocremonini664@gmail.com>
2026-06-16 19:43:31 -0300

    horario com outras classes nao funcional

--- src/modelo/Horario.java
+++ src/modelo/Horario.java
@@ -1,10 +1,21 @@
 public class Horario {
-    private String dia;
-    private String hora;
+    enum DiaSemana {
+        SEGUNDA, TERÇA, QUARTA, QUINTA, SEXTA
+    }
+    enum Turno{
+        manha_turno1, manha_turno2, tarde_turno1, tarde_turno2, noite_turno1, noite_turno2
+    }
+    private DiaSemana dia;
+    private Turno hora;
+    private Professor Docente;
+    Disciplina disciplina;
 
-    public Horario(String dia, String hora) {
+    public Horario(DiaSemana dia, Turno hora, Professor docente, Disciplina disciplina) {
         this.dia = dia;
         this.hora = hora;
+        Docente = docente;
+        this.disciplina = disciplina;
+
     }
 
     public String getDia() {

06def289c79122f3592ecef3c860bb84618cefdf
DreiII664 <diegocremonini664@gmail.com>
2026-06-16 20:03:12 -0300

    turno 35

--- src/modelo/Horario.java
+++ src/modelo/Horario.java
@@ -1,8 +1,10 @@
+package modelo;
+
 public class Horario {
     enum DiaSemana {
         SEGUNDA, TERÇA, QUARTA, QUINTA, SEXTA
     }
-    enum Turno{
+    enum Periodo{
         manha_turno1, manha_turno2, tarde_turno1, tarde_turno2, noite_turno1, noite_turno2
     }
 }

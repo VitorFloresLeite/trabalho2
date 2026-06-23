3b47d9ff980d25bf3f2866ed7ac525cece423b9c
VitorFloresLeite <vitorleite260@gmail.com>
2026-06-22 21:16:16 -0300

    adicionado atributo Turno e ajustado o toString para a exibição

--- src/modelo/Horario.java
+++ src/modelo/Horario.java
@@ -2,10 +2,12 @@ package modelo;
 
 public class Horario {
     private Dias dia;
+    private Turnos turno;
     private Periodos periodo;
 
-    public Horario(Dias dia, Periodos periodo) {
+    public Horario(Dias dia, Turnos turno, Periodos periodo) {
         this.dia = dia;
+        this.turno = turno;
         this.periodo = periodo;
     }
 
@@ -17,6 +19,14 @@ public class Horario {
         this.dia = dia;
     }
 
+    public Turnos getTurno() {
+        return turno;
+    }
+
+    public void setTurno(Turnos turno) {
+        this.turno = turno;
+    }
+
     public Periodos getPeriodo() {
         return periodo;
     }
@@ -24,8 +34,8 @@ public class Horario {
     public void setPeriodo(Periodos periodo) {
         this.periodo = periodo;
     }
-
-    public String descricao() {//Não optei por utilizar void pois a informação não será exibida no console
-        return getDia().toString() + " - " + getPeriodo().toString();
+    @Override
+    public String toString() {//Não optei por utilizar void pois a informação não será exibida no console
+        return getDia().toString() + " - " + getTurno().toString() + " - " + getPeriodo().toString();
     }
 }

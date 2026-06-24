fa4fd6569648d5421b304a0b357f7dd43a8784d4
VitorFloresLeite <vitorleite260@gmail.com>
2026-06-24 01:18:32 -0300

    criada classe abstrata Janela para definir atributos padrão das janelas da aplicação

---
+++ src/Interface/Janela.java
@@ -0,0 +1,51 @@
+package Interface;
+
+import java.awt.*;
+import javax.swing.JFrame;
+
+public abstract class Janela extends JFrame {
+    protected int width = 1920/2;
+    protected int height = 1080/2;
+    protected int offset = 10;
+    protected boolean visible = true;
+    protected boolean resizable = true;
+
+    public Janela(String titulo) {
+        super(titulo);
+        configurarJanela();
+        inicializarComponentes();
+        setVisible(visible);
+    }
+
+    private void configurarJanela() {
+        setSize(width, height);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout(offset, offset));
+        setResizable(resizable);
+        getContentPane().setBackground(new Color(176, 11, 105)); 
+    }
+
+    protected abstract void inicializarComponentes();
+
+    protected void setWindowSize(int width, int height) {
+        this.width = width;
+        this.height = height;
+        setSize(width, height);
+    }
+
+    protected void setWindowOffset(int offset) {
+        this.offset = offset;
+        setLayout(new BorderLayout(offset, offset));
+    }
+
+    protected void setWindowVisibility(boolean visible) {
+        this.visible = visible;
+        setVisible(visible);
+    }
+
+    protected void setWindowResizable(boolean resizable) {
+        this.resizable = resizable;
+        setResizable(resizable);
+    }
+}

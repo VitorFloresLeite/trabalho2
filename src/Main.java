06c4d06b737fb8f87c2ebb23d35a47e8f03068ba
VitorFloresLeite <vitorleite260@gmail.com>
2026-07-01 00:42:31 -0300

    Apenas a criação da janela principal e do controlador são necessários atualmente

--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,13 @@
+import Controle.*;
 import Interface.*;
 
 public class Main {
-    // Offset editável da tabela em relação ao topo (em pixels)
-    // 
     
-    private static Janela frame;
-
+    private static ControlePaineis controlePaineis;
+    private static JanelaPrincipal janelaPrincipal;
     public static void main(String[] args) {
-        frame = new JanelaInicial("janela de teste"); 
-        //SwingUtilities.invokeLater(MainWindow::createAndShowGUI);
+        janelaPrincipal = new JanelaPrincipal("Janela Principal");
+        controlePaineis = new ControlePaineis(janelaPrincipal);
+        janelaPrincipal.setControle(controlePaineis);
     }
-
-    //private static void createAndShowGUI() {
-        //frame = new JanelaInicial();
-        // frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        // JButton editAlocacaoButton = new JButton("Editar Alocação");
-
-        // CriarTabelaEPainel();
-
-        // //JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.CENTER, 10, 10));
-        // //buttonPanel.add(editAlocacaoButton);
-        // //frame.add(buttonPanel, BorderLayout.SOUTH);
-        // Scanner scanner = new Scanner(System.in);
-        
-        
-        // frame.setSize(1000, 650);
-        // frame.setLocationRelativeTo(null);
-        // for(int i=0;i<10;i++){
-            
-        //     int num=scanner.nextInt();
-        //     scanner.nextLine(); // Consume the newline character
-        //     if(num==1){
-        //         frame.setVisible(true);
-        //     }else{
-        //         frame.setVisible(false);
-        //     }
-        // }
-        // scanner.close();
-    //}
-
-    // private  void CriarTabelaEPainel(){
-    //     
-    //}
 }

c78620b6131e26f4875bf43cbc4d146b6ef0f2c5
VitorFloresLeite <vitorleite260@gmail.com>
2026-07-01 21:36:12 -0300

    adicionados novos paineis

--- src/Interface/PaineisDoPrograma.java
+++ src/Interface/PaineisDoPrograma.java
@@ -3,5 +3,8 @@ package Interface;
 public enum PaineisDoPrograma {
     INICIAL,
     GRADE,
-    CADASTRO_PROFESSOR
+    CADASTRO_PROFESSOR,
+    CADASTRO_TURMA,
+    EXIBIR_TURMAS,
+    EXIBIR_PROFESSORES
 }

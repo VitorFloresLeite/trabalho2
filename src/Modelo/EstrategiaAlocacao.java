80006aeff3fc3fd964ec24ed071f12c365b87447
VitorFloresLeite <vitorleite260@gmail.com>
2026-07-02 02:18:33 -0300

    horario não mais necessário

--- src/Modelo/EstrategiaAlocacao.java
+++ src/Modelo/EstrategiaAlocacao.java
@@ -3,5 +3,5 @@ package Modelo;
 import java.util.List;
 
 public interface EstrategiaAlocacao {
-    Grade gerar(List<Disciplina> disciplinas, List<Professor> professores, List<Turma> turmas, List<Horario> horarios);
+    Grade gerar(List<Disciplina> disciplinas, List<Professor> professores, List<Turma> turmas);
 }
